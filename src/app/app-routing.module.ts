3bfa4fcfa7840dc7a6e0a896dc45c958c345c5f5
Javier Solares Servent <javiss1254@hotmail.com>
2018-09-30 15:05:25 -0600

    Se añadió un arreglo de rutas al módulo de ruteo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PokemonesComponent } from './pokemones/pokemones.component';
+
+const rutas: Routes = [
+  { path: 'pokemones', component: PokemonesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
