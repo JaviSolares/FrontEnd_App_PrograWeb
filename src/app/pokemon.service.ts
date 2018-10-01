9fff8436dcba2aa256c58eb5b4083ab690ae89d6
Javier Solares Servent <javiss1254@hotmail.com>
2018-09-30 23:12:43 -0600

    Se implementó una función de manejo de errores en el servicio Pokemon.

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -3,6 +3,7 @@ import { Pokemon } from './pokemon';
 import { Observable, of } from 'rxjs';
 import { MensajeService } from './mensaje.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,34 @@ export class PokemonService {
   /** GET pokemones from the server */
   getPokemones (): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>(this.pokemonesUrl)
+      .pipe(
+        catchError(this.handleError('getPokemones', []))
+      );
   }
 
   getPokemon(id: number): Observable<Pokemon> {
     return of(POKEMONES.find(pokemon => pokemon.id === id));
   }
+
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 
   /** Log a PokemonService message with the MensajeService */
   private log(mensaje: string) {
