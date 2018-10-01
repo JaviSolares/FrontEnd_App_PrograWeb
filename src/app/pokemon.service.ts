269ed68a929f13d5603e0c49793d25c9e80a5ae8
Javier Solares Servent <javiss1254@hotmail.com>
2018-09-30 23:00:14 -0600

    Se agregaron símbolos HTTP al servicio Pokemon y se hizo un log para el servicio de mensajes.

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
-import { POKEMONES } from './mock-pokemones';
 import { Observable, of } from 'rxjs';
 import { MensajeService } from './mensaje.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private pokemonesUrl = 'api/pokemones';  // URL to web api
+  
   getPokemones(): Observable<Pokemon[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.servicioMensaje.add('PokemonService: se recuperaron pokémon');
     return of(POKEMONES);
   }
 
   getPokemon(id: number): Observable<Pokemon> {
-    // TODO: send the message _after_ fetching the pokemon
-    this.servicioMensaje.add(`PokemonService: se recuperó pokémon id=${id}`);
     return of(POKEMONES.find(pokemon => pokemon.id === id));
   }
 
-  constructor(private servicioMensaje: MensajeService) { }
+  /** Log a PokemonService message with the MensajeService */
+  private log(mensaje: string) {
+    this.servicioMensaje.add(`PokemonService: ${mensaje}`);
+  }
+
+  constructor(
+    private http: HttpClient,
+    private servicioMensaje: MensajeService) { }
 }
