795f68326e6b1c2be1f7f15e300d967caf143763
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-08 17:43:13 -0600

    Se modificó el server para que muestre un mensaje de ruta inválida si no se cumple una condición.

--- Lab4_Server/server.js
+++ Lab4_Server/server.js
@@ -1,6 +1,15 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {});
+var server = http.createServer(function(req, res) {
+    if (req.url == 'hello/:name') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ "Hello": "<name>" }));
+        res.end();
+    }
+    else {
+        res.end('Invalid request!');
+    }
+});
 
 server.listen(5000);
 
