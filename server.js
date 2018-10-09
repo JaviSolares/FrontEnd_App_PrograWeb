0ad3f8600ac2bbb767444c1145eb30da28568cf4
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-09 11:11:44 -0600

    Se cambió de ubicación el archivo "app.js" y se creó el archivo "server.js".

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
-    res.send('<html><body><h1>Hello World</h1></body></html>');
+    res.sendFile('index.html');
 });
 
-app.post('/submit-data', function(req, res) {
-    res.send('POST Request');
+app.post('/submit-student-data', function(req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+
+    res.send(name + ' Submitted Succesfully!');
 });
 
 app.put('/update-data', function(req, res) {
