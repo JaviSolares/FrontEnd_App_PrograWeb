b2ea5b74e85cf741514fc70cf8fe39d4467aed16
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-09 11:37:02 -0600

    Se hicieron modificaciones en el archivo "app.js" y se eliminó el archivo "server.js".

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', 
+    { root: 'C:/Users/Javier/Desktop/FrontEnd_App_PrograWeb-master/FrontEnd_App_PrograWeb/' });
+});
+
+app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
 var server = app.listen(5000, function() {
     console.log('Node server is running..');
 });
