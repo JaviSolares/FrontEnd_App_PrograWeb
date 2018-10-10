e8dd3094ba2875f3c80c6e0e258b0845e1c96cb9
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-09 18:54:11 -0600

    Se hicieron modificaciones en el archivo "app.js", y se instaló la extensión TypeScript Require.

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
+// import { stringify } from "@angular/core/src/util";
+
 var express = require('express');
 var app = express();
+var router = express.Router();
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.sendFile('index.html', 
-    { root: 'C:/Users/Javier/Desktop/FrontEnd_App_PrograWeb-master/FrontEnd_App_PrograWeb/' });
-});
+const servidorUrl = '/api/v1/pokemon';
+app.use('/api/v1/pokemon', router);
+
+
+var dataPokemones = require('./src/app/in-memory-data.service.ts');
+const listaPokemon = dataPokemones.pokemones;
+
+function readPokemon() {
+    app.param('id', function(req, res, next, id) {
+        if (id === '')
+        {
+            res.send(200, JSON.stringify(listaPokemon));
+        }
+        else
+        {
+            var pokemon = listaPokemon.find(function(p) {
+                return p.id === id;
+            });
+
+            if (pokemon === undefined)
+            {
+                res.send(404, 'No se encontró ningún pokémon con ese id.');
+            }
+            else
+            {
+                req.id = id;
+                next();
+            }
+        }
+    })
+    
+    app.get('/api/v1/pokemon/:id', function(req, res) {
+        res.sendFile('index.html', 
+        { root: 'C:/Users/Javier/Desktop/FrontEnd_App_PrograWeb-master/FrontEnd_App_PrograWeb/' });
+    });    
+}
+
 
 app.post('/submit-student-data', function (req, res) {
     var name = req.body.firstName + ' ' + req.body.lastName;
