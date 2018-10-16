db4e2b11478608f889a542dea19155d600f10a92
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-15 18:14:03 -0600

    La clase Pokemon se recreó en un archivo .js, pero de momento no se utiliza para nada.

---
+++ pokemon.js
@@ -0,0 +1,9 @@
+class Pokemon {
+    constructor(id, nombre, tipo_prim, tipo_secu, region) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo_prim = tipo_prim;
+        this.tipo_secu = tipo_secu;
+        this.region = region;
+    }
+}
