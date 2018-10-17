f902d4f30c1cf3f527ae4f6cffa31f16e0ccdc76
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-17 01:37:39 -0600

    Se agregó una conexión a la base de datos de MongoDB en el servidor.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// import { stringify } from "@angular/core/src/util";
-
 const express = require('express');
 const app = express();
 const router = require('./rutas/api');
 
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 // app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost/pokedex');
+
 app.use(bodyParser.json());
 
 app.use('/api/v1/pokemon', router);
