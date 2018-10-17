c1a7f68bea79dacd3e4c545c47cbaaaec7eb6890
Javier Solares Servent <javiss1254@hotmail.com>
2018-10-17 17:47:33 -0600

    Se modificaron las 2 alternativas del método GET para retornar todos los documentos de la colección en MongoDB si no se especifica un id, y si se especifica un id retornar únicamente el documento que posea el id solicitado.

--- rutas/api.js
+++ rutas/api.js
@@ -5,34 +5,18 @@ const Monster = require('../modules/pokemon');
 var datos = require('../data');
 var listaPokemon = datos.getLista();
 
-router.get('/', function(req, res) {
-    var jsonLista = JSON.stringify({listaPokemon});
-    res.send(jsonLista);
+router.get('/', function(req, res, next) {
+    Monster.find({}).then(function(pokemones) {
+        res.send(pokemones);
+    });
 });
 
-router.get('/get/:id', function(req, res, next) {
-    var num = Number(req.params.id);
-    /*if (req.params.id === 'add-new')
-    {
-        res.sendFile('pokepost.html', 
-        { root: 'C:/Users/Javier/Desktop/FrontEnd_App_PrograWeb-master/FrontEnd_App_PrograWeb/' });
-    }*/
-    if (!listaPokemon.some(x => x.id === num))
-    {
-        res.send(404, 'No se encontró ningún pokémon con ese id.');
-    }
-    else
-    {
-        var pokemon = listaPokemon.find(x => x.id === num);
-            
-        res.send('Id: ' + pokemon.id + '<br />Nombre: ' +
-        pokemon.nombre + '<br />Tipo Primario: ' + pokemon.tipo_prim
-        + '<br />Tipo Secundario: ' + pokemon.tipo_secu +
-        '<br />Región: ' + pokemon.region);
-    }
-    
+router.get('/:id', function(req, res, next) {
+    Monster.findOne({ id: req.params.id })
+        .then(function(pokemon) {
+            res.send(pokemon);
+        });
 });    
-//}
 
 router.post('/add-new', function (req, res, next) {
     Monster.create(req.body).then(function(pokemon) {
